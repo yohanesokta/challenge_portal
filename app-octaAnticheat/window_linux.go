4360022b37aa31a30f83efe7fe28d2396b2aa66b
yohanesokta <yohanesoktayt72@gmail.com>
2026-05-03 06:27:24 +0700

    feat: add installation script for Octa Anticheat on Linux and enhance window process detection for X11 and Wayland

--- app-octaAnticheat/window_linux.go
+++ app-octaAnticheat/window_linux.go
@@ -1,24 +1,59 @@
 //go:build linux
+
 package main
 
 import (
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
-func getActiveWindowTitle() string {
-	out, err := exec.Command("xdotool", "getactivewindow", "getwindowname").Output()
+func getActiveWindowProcessX11() string {
+	out, err := exec.Command("xdotool", "getactivewindow", "getwindowpid").Output()
 	if err == nil {
-		title := strings.TrimSpace(string(out))
-		if title != "" {
-			return title
+		pid := strings.TrimSpace(string(out))
+		if pid != "" {
+			return pid
+		}
+	}
+	return "Unknown"
+}
+
+func getActiveWindowProcessWayland() string {
+
+	home := os.Getenv("HOME")
+	locationFolder := filepath.Join(home, ".config", "octaAnticheat")
+
+	out, err := exec.Command(locationFolder+"/kdotool", "getactivewindow").Output()
+	var windowid string
+	if err == nil {
+		pid := strings.TrimSpace(string(out))
+		if pid != "" {
+			windowid = pid
 		}
 	}
 
-	if strings.Contains(os.Getenv("XDG_SESSION_TYPE"), "wayland") {
-		return "Linux (Wayland) - Restricted"
+	if windowid != "" {
+		out, err = exec.Command(locationFolder+"/kdotool", "getwindowname", windowid).Output()
+		if err == nil {
+			pid := strings.TrimSpace(string(out))
+			if pid != "" {
+				return pid
+			}
+		}
 	}
-
 	return "Unknown"
+}
+
+func getActiveWindowTitle() string {
+
+	if strings.Contains(os.Getenv("XDG_SESSION_TYPE"), "wayland") {
+
+		println("Wayland session detected")
+		return getActiveWindowProcessWayland()
+	} else {
+		println("X11 session detected")
+		return getActiveWindowProcessX11()
+	}
 }
